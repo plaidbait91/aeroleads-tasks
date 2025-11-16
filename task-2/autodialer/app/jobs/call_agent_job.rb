022f63a2f81b98106618074f36d9ca1a44301fc8
plaidbait91 <f20200055@hyderabad.bits-pilani.ac.in>
2025-11-17 00:59:49 +0530

    delete unused job

--- task-2/autodialer/app/jobs/call_agent_job.rb
+++
@@ -1,8 +0,0 @@
-class CallAgentJob < ApplicationJob
-  queue_as :default
-
-  def perform(input:, tool_strings:)
-    agent = AgentService.new
-    agent.call(input: input, tool_strings: tool_strings)
-  end
-end
